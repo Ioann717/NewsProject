59f1a4e40d964ba20a3572fb06799f7df9a01082
Ioann <78559802+Ioann717@users.noreply.github.com>
2023-09-08 15:47:35 +0300

    Update urls.py

--- NewsProject/NewsPortal/urls.py
+++ NewsProject/NewsPortal/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
-    path('', include('news.urls'), name='posts'),
+    path('pages/', include('django.contrib.flatpages.urls')),
+   # Делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py)
+   # подключались к главному приложению с префиксом products/.
+   path('newsportal/', include('newsportal.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
+    #path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
